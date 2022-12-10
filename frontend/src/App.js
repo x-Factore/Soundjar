bb9c8d22f598fc8ae9b5eba183923d09844cac6c
AbdennourBelkadi <ekatzu62@gmail.com>
2022-12-10 15:24:43 +0100

    commit 2 i guess

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,48 @@
-import './App.css';
-import Navbar from "./components/Navbar"
-import posts from './data/example-data'
-import AllPosts from './components/AllPosts'
-import OnePost from './components/OnePost';
+import "./App.css";
+import Navbar from "./components/Navbar";
+// import posts from './data/example-data'
+import AllPosts from "./components/AllPosts";
+// import OnePost from './components/OnePost';
+import React, { useEffect, useState } from "react";
+import About from "./components/About";
+import Services from "./components/Services";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
-  console.log(posts);
+  // console.log("iiiiiiiiiiiiiiiiiiiiii",posts);
+
+  const [connected, setConnected] = useState(false);
+  const checkToken = () => {
+    if (typeof window !== "undefined") {
+      const user = JSON.parse(localStorage.getItem("token"));
+      if (user) {
+        setConnected(true);
+      } else {
+        setConnected(false);
+      }
+    }
+  };
+  useEffect(() => {
+    checkToken();
+  }, [connected]);
+  const logout = () => {
+    localStorage.clear();
+  };
   return (
     <div className="App">
-      <Navbar />
-      <AllPosts data={posts} />
+      <Navbar  />
+      <Routes>
+        <Route index element={<Login />} />
+
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<AllPosts />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Services />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
-    
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,77 @@
-export default function Navbar(){
-    return (
-        <nav className="nav">
-            <a href='/' class='site-title'>Soundjar</a>
-            <ul>
-                <li>
-                    <a href='/account'>Account</a>
-                </li>
-                <li>
-                    <a href='/newpost'>New Post</a>
-                </li>
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+function Navbar({ user, logout }) {
+  const navigate = useNavigate();
+  const logoutHandler = () => {
+    logout();
+    window.location.reload(false);
+    navigate("/login");
+  };
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light shadow">
+        <div class="container">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link class="nav-link active" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" to="/service">
+                  Services
+                </Link>
+              </li>
             </ul>
-        </nav>
-    )
+            <a class="navbar-brand fw-bolder fs-4 mx-auto">Soundjar</a>
+            {!user ? (
+              <>
+                <Link
+                  className="btn btn-outline-primary ms-auto px-4 rounded-pill"
+                  to="/login"
+                >
+                  <i className="fa fa-sign-in ms-2 "></i> Login
+                </Link>
+                <Link
+                  className="btn btn-outline-primary ms-2 px-4 rounded-pill"
+                  to="/register"
+                >
+                  <i className="fa fa-user-plus me-2"></i>Register
+                </Link>
+              </>
+            ) : (
+              <Link
+                className="btn btn-outline-primary ms-2 px-4 rounded-pill"
+                to="/register"
+                onClick={logoutHandler}
+              >
+                <i className="fa fa-sign-out"></i>
+                Logout
+              </Link>
+            )}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
+
+export default Navbar;
